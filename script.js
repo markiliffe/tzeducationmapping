0b5a72f811abc5d45763023846b44da6077de0d3
Mark Iliffe <mark@markiliffe.co.uk>
2013-05-09 20:08:27 +0100

    syncing repo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var TileJSONs = [
     'http://a.tiles.mapbox.com/v3/examples.map-20v6611k,mapbox.dc-property-values.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-building.jsonp',
+    'http://a.tiles.mapbox.com/v3/markiliffe.Necta_Secondary_Percentage_Change_2011_2012.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-construction.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-crime.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-robbery.jsonp',
@@ -12,7 +12,7 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
 
     // Assign readable names to all layers
     map.getLayerAt(0).named('base');
-    map.getLayerAt(1).named('building');
+    map.getLayerAt(1).named('secondary_change');
     map.getLayerAt(2).named('construction');
     map.getLayerAt(3).named('crime');
     map.getLayerAt(4).named('robbery');
@@ -23,7 +23,7 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
     map.getLayer('base').composite(false);
 
     // Disable all overlay layers by default
-    map.disableLayer('building');
+    map.disableLayer('secondary_change');
     map.disableLayer('construction');
     map.disableLayer('crime');
     map.disableLayer('robbery');
@@ -32,9 +32,9 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
 
     // Set initial latitude, longitude and zoom level
     map.setCenterZoom({
-        lat: 38.90900,
-        lon: -77.01439
-    }, 13);
+        lat: -8.7737,
+        lon: 35.4199
+    }, 6);
 
     // Set minimum and maximum zoom levels
     map.setZoomRange(0, 15);
