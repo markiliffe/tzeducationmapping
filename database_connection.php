ba77eecda3372590eaed265f8ef2e39ee517f6b7
Mark Iliffe <mark@markiliffe.co.uk>
2013-05-25 19:57:53 +0300

    changed the database name, hopefully correct now!

--- database_connection.php
+++ database_connection.php
@@ -5,5 +5,5 @@
  <!--Heroku Details: mysql://be2a6a862030db:51cc303e@us-cdbr-east-03.cleardb.com/heroku_1c814cc48ffb566?reconnect=true -->
 <?php
 $con=mysql_connect('us-cdbr-east-03.cleardb.com','be2a6a862030db','51cc303e');
-$db=mysql_select_db('brn',$con);
+$db=mysql_select_db('heroku_1c814cc48ffb566',$con);
 ?>
